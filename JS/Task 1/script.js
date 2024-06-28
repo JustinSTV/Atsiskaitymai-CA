ca1a210faf611985ce61e40e0e7b2034b4f3ce19
Justin S <steponaviciusjustas@gmail.com>
2024-06-28 08:55:17 +0300

    Task 1 Done

--- JS/Task 1/script.js
+++ JS/Task 1/script.js
@@ -9,3 +9,29 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
+document
+    .querySelector('form')
+    .addEventListener('submit', e => {
+        e.preventDefault()
+
+        const weightInput = e.target.elements.search.value;
+        console.log(weightInput)
+
+        let pounds = weightInput * 2.2046
+        let grams = weightInput / 0.0010000
+        let ounces = weightInput * 35.274
+
+        const outputDiv = document.querySelector('#output')
+        outputDiv.innerHTML = ''
+
+        const poundsDiv = document.createElement('div');
+        poundsDiv.textContent = `Pounds: ${pounds.toFixed(2)} lb`;
+
+        const gramsDiv = document.createElement('div');
+        gramsDiv.textContent = `Grams: ${grams.toFixed(0)} g`;
+
+        const ouncesDiv = document.createElement('div');
+        ouncesDiv.textContent = `Ounces: ${ounces.toFixed(2)} oz`;
+
+        outputDiv.append(poundsDiv, gramsDiv, ouncesDiv)
+    })
