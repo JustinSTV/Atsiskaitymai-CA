7ce48faae9d274713f0a3dd4539feadb4c450e2b
Justin S <steponaviciusjustas@gmail.com>
2024-06-28 09:59:26 +0300

    Task 4 Done

--- JS/Task 4/script.js
+++ JS/Task 4/script.js
@@ -10,3 +10,28 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+  .then(res => res.json())
+  .then(cars => {
+    // console.log(cars)
+    const output = document.querySelector('#output')
+    cars.forEach(car => {
+      // console.log(car)
+      const brandCard = document.createElement('div')
+      brandCard.classList.add('carCard')
+
+      const carBrandName = document.createElement('h3')
+      carBrandName.textContent = car.brand
+      brandCard.appendChild(carBrandName)
+
+      // console.log(car.models)
+      car.models.forEach(carModel => {
+        // console.log(carModel)
+        const carModelName = document.createElement('p')
+        carModelName.textContent = carModel
+        brandCard.appendChild(carModelName)
+      })
+      output.appendChild(brandCard)
+    });
+  })
