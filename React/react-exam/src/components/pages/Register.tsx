30e5d01df9b8ff315ffe582211e8723d8411cb99
Justin S <steponaviciusjustas@gmail.com>
2024-09-05 16:44:23 +0300

    Registrations works with validations

--- React/react-exam/src/components/pages/Register.tsx
+++ React/react-exam/src/components/pages/Register.tsx
@@ -20,7 +20,79 @@ const Register = () => {
     profilePicture: ''
   })
 
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<{
+    username: string;
+    email: string;
+    password: string;
+    passwordRepeat: string;
+    dob: string;
+    userExists: string;
+    hasError: boolean;
+  }>({
+    username: '',
+    email: '',
+    password: '',
+    passwordRepeat: '',
+    dob: '',
+    userExists: '',
+    hasError: false
+  });
+
+  const registerErrorChecking: {
+    [key: string]: (input: string, password?: string) => string;
+  } = {
+    username: (input: string) => {
+      if(input.length < 3){
+        return 'Must be longet than 3';
+      } else if(input.length >= 10){
+        return 'Must be shorter than 10';
+      } else if(!input){
+        return 'Field must be filled';
+      } else {
+        return '';
+      }
+    },
+    email: (input: string) => {
+      if (!input) {
+        return 'Email is required';
+      } else {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(input)) {
+          return 'Invalid email format';
+        } else {
+          return '';
+        }
+      }
+    },
+    //trumpas checkingas del testing purpose
+    password: (input: string) => {
+      if (input.length < 3) {
+        return 'Password must be at least 3 characters long';
+      } else if (!/[A-Z]/.test(input)) {
+        return 'Password must contain at least one uppercase letter';
+      } else {
+        return '';
+      }
+    },
+    passwordRepeat: (input: string, password?: string) => {
+      if (input !== password) {
+        return 'Passwords do not match';
+      } else {
+        return '';
+      }
+    },
+    dob: (input: string) => {
+      const userAge = new Date().getFullYear() - new Date(input).getFullYear();
+      if (!input) {
+        return 'Date of birth is required';
+      } else if (userAge < 18) {
+        return 'You must be at least 18 years old to register';
+      } else {
+        return '';
+      }
+    },
+  }
+  
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputFields({
@@ -29,38 +101,61 @@ const Register = () => {
     })
   }
 
+  const blurHandle = (event: React.FocusEvent<HTMLInputElement>) => {
+
+    const fieldName = event.target.name;
+    console.log(fieldName)
+    const validateField = registerErrorChecking[fieldName]
+
+    if(fieldName){
+      setError({
+        ...error,
+        [fieldName]: validateField(event.target.value, inputFields.password),
+      })
+    } else{
+      console.error(`No validation function found for ${fieldName}`);
+    }
+  }
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (inputFields.password !== inputFields.passwordRepeat) {
-      setError("Passwords do not match");
-      return;
+    const userExists = user.find(
+      (user) => user.username === inputFields.username || user.email === inputFields.email
+    )
+    if(userExists){
+      setError({
+        ...error,
+        userExists: "Username or email is already taken",
+        hasError: true
+      })
     }
 
-    const userExists = user.some(user => user.email === inputFields.email || user.username === inputFields.username);
-    if (userExists) {
-      setError("User already exists");
-      return;
+    if(inputFields.password === inputFields.passwordRepeat){
+
+      const defaultProfilePicture = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png"
+
+      const newUser = {
+        id: generateID(),
+        username: inputFields.username,
+        email: inputFields.email,
+        password: bcrypt.hashSync(inputFields.password, 10),
+        passwordRepeat: inputFields.passwordRepeat,
+        passwordVisable: inputFields.passwordRepeat,
+        dob: inputFields.dob,
+        profilePicture: inputFields.profilePicture || defaultProfilePicture
+      }
+
+      addNewUser(newUser);
+      setLoggedInUser(newUser)
+      navigate('/')
+    } else{
+      setError({
+        ...error,
+        hasError: true
+      })
     }
 
-    const hashedPassword = bcrypt.hashSync(inputFields.password, 10);
-
-    const defaultProfilePicture = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png"
-
-    const newUser = {
-      id: generateID(),
-      username: inputFields.username,
-      email: inputFields.email,
-      password: hashedPassword,
-      passwordRepeat: inputFields.passwordRepeat,
-      passwordVisable: inputFields.passwordRepeat,
-      dob: inputFields.dob,
-      profilePicture: inputFields.profilePicture || defaultProfilePicture
-    }
-
-    addNewUser(newUser);
-    setLoggedInUser(newUser),
-    navigate('/')
   }
 
   return (
@@ -74,7 +169,10 @@ const Register = () => {
             name="username" id="username"
             value={inputFields.username}
             onChange={handleInputChange}
+            onBlur={blurHandle}
+            required
           />
+          {error.username && <p>{error.username}</p>}
         </div>
         <div>
           <label htmlFor="email">Email:</label>
@@ -83,7 +181,10 @@ const Register = () => {
             id='email' name='email'
             value={inputFields.email}
             onChange={handleInputChange}
+            onBlur={blurHandle}
+            required
           />
+          {error.email && <p>{error.email}</p>}
         </div>
         <div>
           <label htmlFor="password">Password:</label>
@@ -92,7 +193,10 @@ const Register = () => {
             name="password" id="password"
             value={inputFields.password}
             onChange={handleInputChange}
+            onBlur={blurHandle}
+            required
           />
+          {error.password && <p>{error.password}</p>}
         </div>
         <div>
           <label htmlFor="passwordRepeat">Password Repeat:</label>
@@ -101,7 +205,10 @@ const Register = () => {
             name="passwordRepeat" id="passwordRepeat"
             value={inputFields.passwordRepeat}
             onChange={handleInputChange}
+            onBlur={blurHandle}
+            required
           />
+          {error.passwordRepeat && <p>{error.passwordRepeat}</p>}
         </div>
         <div>
           <label htmlFor="dob">Date of Birth:</label>
@@ -110,7 +217,10 @@ const Register = () => {
             name='dob' id='dob'
             value={inputFields.dob}
             onChange={handleInputChange}
+            onBlur={blurHandle}
+            required
           />
+          {error.dob && <p>{error.dob}</p>}
         </div>
         <div>
           <label htmlFor="profilePicture">Profile Picture:</label>
@@ -123,7 +233,7 @@ const Register = () => {
         </div>
         <input type="submit" value="Create Account"/>
       </form>
-      {error && <p>{error}</p>}
+      {error.userExists && <p>{error.userExists}</p>}
       <p>Have an Account already?<Link to="/login">Login</Link></p>
     </section>
   );
