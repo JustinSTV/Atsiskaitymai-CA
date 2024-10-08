a5354b01262b3b790f8983aa27cf7e068924db4e
Justin S <steponaviciusjustas@gmail.com>
2024-10-08 11:20:53 +0300

    [server] sorting done

--- NodeJS/server/index.js
+++ NodeJS/server/index.js
@@ -15,3 +15,38 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+
+app.get('/allBooks', async (req, res) => {
+  const settings = {
+    filter: {},
+    order: {},
+    skip: 0,
+    limit: 10,
+  };
+
+  console.log('Query parameters:', req.query);
+
+  Object.keys(req.query).forEach(key => {
+    const keyValue = key.split('_')[0]; //? sort arba filter
+    if (keyValue === 'sort') {
+      //?pasiemu sort
+      //? 1 = asc, -1 desc
+      settings.order = { [key.split('_')[1]]: Number(req.query[key]) } 
+      console.log('sorting', settings.order = { [key.split('_')[1]]: Number(req.query[key]) })
+    }
+
+  });
+
+  console.log("apply'inti settings", settings);
+
+  const client = await MongoClient.connect(CONNECT_URL);
+
+  const data = await client.db('atsiskaitymas').collection('books').aggregate([
+    Object.keys(settings.order).length ? { $sort: settings.order } : { $sort: { title: 1 } },
+    { $skip: settings.skip },
+    { $limit: settings.limit },
+  ]).toArray();
+
+  res.send(data);
+  client.close();
+});
